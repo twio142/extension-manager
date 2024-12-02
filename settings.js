c204a4460fd948487f7cb73b8b466e90395ccdad
twio142 <64556708+twio142@users.noreply.github.com>
2024-12-02 07:30:18 +0100

    Add radio buttons for vim and emacs modes in settings.html
    * Add event listeners for the vim and emacs radio buttons
    * Save the selected mode to chrome storage
    * Retrieve the saved mode from chrome storage and set the radio buttons accordingly

--- settings.js
+++ settings.js
@@ -18,4 +18,34 @@ button.addEventListener("click", () => {
 chrome.storage.local.get("externalUrl", (data) => {
   document.querySelector("#external-url").value = data.externalUrl || "";
 });
-// TODO: set `initVimMode` based on the checkboxes
+
+const vimRadio = document.querySelector("#vim-mode");
+const emacsRadio = document.querySelector("#emacs-mode");
+
+vimRadio.addEventListener("change", () => {
+  if (vimRadio.checked) {
+    emacsRadio.checked = false;
+    chrome.storage.local.set({ initWithVim: true });
+  } else {
+    chrome.storage.local.set({ initWithVim: false });
+  }
+});
+
+emacsRadio.addEventListener("change", () => {
+  if (emacsRadio.checked) {
+    vimRadio.checked = false;
+    chrome.storage.local.set({ initWithVim: false });
+  } else {
+    chrome.storage.local.set({ initWithVim: true });
+  }
+});
+
+chrome.storage.local.get("initWithVim", (data) => {
+  if (data.initWithVim) {
+    vimRadio.checked = true;
+    emacsRadio.checked = false;
+  } else {
+    vimRadio.checked = false;
+    emacsRadio.checked = true;
+  }
+});
